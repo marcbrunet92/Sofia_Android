703268b8e806a81b706f5adbd71d8c006dac7f73
marcb <marcbrunet92@gmail.com>
2026-06-01 22:15:19 +0200

    add aab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.lemarc.sofia"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
