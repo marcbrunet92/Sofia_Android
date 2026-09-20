e3ec6b5c08bc1d819852cba1321ba9237287c281
Marc Brunet <marcbrunet92@gmail.com>
2026-09-20 22:37:20 +0200

    Update GraphScreen.kt

--- app/src/main/java/com/lemarc/sofia/ui/graph/GraphScreen.kt
+++ app/src/main/java/com/lemarc/sofia/ui/graph/GraphScreen.kt
@@ -1,5 +1,11 @@
 package com.lemarc.sofia.ui.graph
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color as ComposeColor
 import android.graphics.Color
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
